abbd4dafa667242adfa8a65b0d42b4efc694e45f
martin378rm <martinrivaldo112@gmail.com>
2023-07-09 18:34:26 +0700

    inital commit 9/7/23

--- array.go
+++ array.go
@@ -2,12 +2,19 @@ package main
 
 import "fmt"
 
-func main () {
+func main() {
 	var names [4]string
-		names[0] = "trafalgar"
-		names[1] = "d"
-		names[2] = "water"
-		names[3] = "law"
-		fmt.Println(names[0], names[1], names[2], names[3])
+	names[0] = "martin"
+	names[1] = "rivaldo"
+	names[2] = "pardamean"
+	names[3] = "manurung"
+	names[0] = "hello"
+	fmt.Println(names[0], names[1], names[2], names[3])
+	fmt.Println("hello")
+
+
+
+
+
 
 }
