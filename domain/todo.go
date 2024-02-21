0c7a18b5a0b1512522b55bd156310418868a06e4
martin378rm <martinrivaldo112@gmail.com>
2024-02-21 22:54:42 +0700

    rest

--- domain/todo.go
+++ domain/todo.go
@@ -1,6 +1,21 @@
 package domain
 
+import "log"
+
 type Todo struct {
 	ID   int64  `json:"id"`
 	Task string `json:"task"`
+}
+
+func (todo *Todo) String() string {
+	return todo.Task
+}
+
+func main() {
+	t := Todo{
+		ID:   1,
+		Task: "Go",
+	}
+	log.Println(t.String())
+
 }
